8dc7a34e12d5d5c18afcc92ab0d24764aad91465
Parallax18 <rhythmist204@gmail.com>
2022-04-19 23:02:36 +0100

    Project setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header/Header';
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Header /> */}
+      <h1>Let's buld gmail</h1>
+    </div>
+  );
+}
+
+export default App;
